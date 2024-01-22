0de7946d8417fb865c4c808fd0de7801e8da428b
tutsi <tuta.matea@gmail.com>
2024-01-22 16:08:59 +0100

    Add hover effect that changes colour

--- javascript.js
+++ javascript.js
@@ -9,5 +9,14 @@ for (i = 0; i < 16; i++) {
         let divRow = document.createElement("div");
         divRow.setAttribute("style", "border: solid 1px black; margin: 0; padding: 0; flex: 1;");
         divColumn.appendChild(divRow);
+        divRow.classList.add("hover");
     }
 }
+
+let hoverElements = document.querySelectorAll(".hover");
+
+hoverElements.forEach(function(hoverElement) {
+    hoverElement.addEventListener("mouseover", function() {
+        hoverElement.style.backgroundColor = "purple";
+    })
+})
